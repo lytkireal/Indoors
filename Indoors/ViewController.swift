3290d7ddcc16d8969c03b1bda24e3d4d338497d2
Artem Lytkin <lytkinreal@mail.ru>
2017-08-23 18:02:08 +0300

    Initial Commit

---
+++ Indoors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Indoors
+//
+//  Created by macbook air on 23/08/2017.
+//  Copyright © 2017 Lytkin Artem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
